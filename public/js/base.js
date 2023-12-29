ba23cd08302c9147a0dae5c0f21556d3c7ef1174
Queirozzzzzzzzzz <vinicius.queiroz.coding@gmail.com>
2023-12-29 11:23:34 -0300

    add responsiveness to `landing-page.ejs`

---
+++ public/js/base.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    const menuItem = document.querySelector("#menu");
+    const navbarItems = document.querySelector(".navbar-items");
+   
+    // Activate or deactivate navbar items
+    function toggleNavbarItems() {
+      if (navbarItems.style.display === "none" || navbarItems.style.display == "") {
+        navbarItems.style.display = "block";
+      } else {
+        navbarItems.style.display = "none";
+      }
+    }
+   
+    // Event listener for navbar items
+    if (menuItem && navbarItems) {
+      menuItem.addEventListener("click", toggleNavbarItems);
+    }
+   };
+   
