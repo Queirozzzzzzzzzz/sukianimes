68f1be502fdcb8ec61abe54ad04188de84faaec8
Queirozzzzzzzzzz <vinicius.queiroz.coding@gmail.com>
2023-12-30 07:46:52 -0300

    fix `header css`

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 // imports
-const express = require('express')
-const app = express()
-const port = 3592
+const express = require("express");
+const app = express();
+const port = 3592;
 
 // Static Files
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
-app.use('/svg', express.static(__dirname + 'public/svg'))
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/img", express.static(__dirname + "public/img"));
+app.use("/svg", express.static(__dirname + "public/svg"));
 
 // Set Views
-app.set('views', './views')
-app.set('view engine', 'ejs')
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-app.get('', (req, res) => {
-    res.render('landing-page')
-})
+app.get("", (req, res) => {
+  res.render("landing-page");
+});
 
-app.get('/login', (req, res) => {
-    res.render('login')
-})
-
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
 // Listen on port 3592
-app.listen(port, () => console.info(`Listening on port ${port}`))
+app.listen(port, () => console.info(`Listening on port ${port}`));
--- public/js/base.js
+++ public/js/base.js
@@ -1,19 +1,21 @@
-window.onload = function() {
-    const menuItem = document.querySelector("#menu");
-    const navbarItems = document.querySelector(".navbar-items");
-   
-    // Activate or deactivate navbar items
-    function toggleNavbarItems() {
-      if (navbarItems.style.display === "none" || navbarItems.style.display == "") {
-        navbarItems.style.display = "block";
-      } else {
-        navbarItems.style.display = "none";
-      }
+window.onload = function () {
+  const menuItem = document.querySelector("#menu");
+  const navbarItems = document.querySelector(".navbar-items");
+
+  // Activate or deactivate navbar items
+  function toggleNavbarItems() {
+    if (
+      navbarItems.style.display === "none" ||
+      navbarItems.style.display == ""
+    ) {
+      navbarItems.style.display = "block";
+    } else {
+      navbarItems.style.display = "none";
     }
-   
-    // Event listener for navbar items
-    if (menuItem && navbarItems) {
-      menuItem.addEventListener("click", toggleNavbarItems);
-    }
-   };
-   
+  }
+
+  // Event listener for navbar items
+  if (menuItem && navbarItems) {
+    menuItem.addEventListener("click", toggleNavbarItems);
+  }
+};
