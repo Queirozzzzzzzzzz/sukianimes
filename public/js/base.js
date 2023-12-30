ebee3a189ac0e88bb73e273c39df2bd330ce8f25
Queirozzzzzzzzzz <vinicius.queiroz.coding@gmail.com>
2023-12-30 09:10:25 -0300

    fix minor `responsiveness design`

--- public/js/base.js
+++ public/js/base.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   const menuItem = document.querySelector("#menu");
   const navbarItems = document.querySelector(".navbar-items");
+  const header = document.querySelector("header");
 
   // Activate or deactivate navbar items
   function toggleNavbarItems() {
@@ -9,8 +10,10 @@ window.onload = function () {
       navbarItems.style.display == ""
     ) {
       navbarItems.style.display = "block";
+      header.style.position = "fixed";
     } else {
       navbarItems.style.display = "none";
+      header.style.position = "absolute";
     }
   }
 
