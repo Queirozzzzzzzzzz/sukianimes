0bdd4803eed33dfad34b03a27c79c05bcd8dd700
Queirozzzzzzzzzz <vinicius.queiroz.coding@gmail.com>
2024-01-02 12:34:19 -0300

    `ejs/css hard reset`

--- public/js/base.js
+++ public/js/base.js
@@ -1,21 +1,23 @@
 window.onload = function () {
-  const menuItem = document.querySelector("#menu");
+  const body = document.querySelector("body");
+  const navbarItem = document.querySelector("#navbar-icon");
   const navbarItems = document.querySelector(".navbar-items");
 
-  // Activate or deactivate navbar items
-  function toggleNavbarItems() {
-    if (
-      navbarItems.style.display === "none" ||
-      navbarItems.style.display == ""
-    ) {
-      navbarItems.style.display = "flex";
+  // Activate or deactivate navbar items and page scroll
+  function togglenavbarItems() {
+    if (!navbarItems.classList.contains("active")) {
+      navbarItem.classList.add("active");
+      navbarItems.classList.add("active");
+      body.style.overflow = "hidden";
     } else {
-      navbarItems.style.display = "none";
+      navbarItem.classList.remove("active");
+      navbarItems.classList.remove("active");
+      body.style.overflow = "visible";
     }
   }
 
   // Event listener for navbar items
-  if (menuItem && navbarItems) {
-    menuItem.addEventListener("click", toggleNavbarItems);
+  if (navbarItem && navbarItems) {
+    navbarItem.addEventListener("click", togglenavbarItems);
   }
 };
