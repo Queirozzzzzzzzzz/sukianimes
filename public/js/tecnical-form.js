3acf320449ebb6edce63dfc19c019946d56f4149
Queirozzzzzzzzzz <vinicius.queiroz.coding@gmail.com>
2024-01-03 10:28:26 -0300

    add `login and register`

--- public/js/tecnical-form.js
+++ public/js/tecnical-form.js
@@ -1,21 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  var inputs = ["email", "password"];
+  var inputs = ["username", "email", "password", "password-1", "password-2"];
 
   for (let i = 0; i < inputs.length; i++) {
     let inputId = inputs[i];
-    document
-      .querySelector(`#${inputId}`)
-      .addEventListener("input", function () {
-        let labelElement = document.querySelector(`label[for='${inputId}']`);
-        if (this.value !== "") {
-          labelElement.style.fontSize = "1.2em";
-          labelElement.style.color = "var(--purple-second)";
-          labelElement.style.marginTop = "0vh";
-        } else {
-          labelElement.style.fontSize = "";
-          labelElement.style.color = "";
-          labelElement.style.margin = "";
-        }
-      });
+    let inputElement = document.getElementById(inputId);
+
+    if (inputElement) {
+      document
+        .querySelector(`#${inputId}`)
+        .addEventListener("input", function () {
+          let input = document.getElementById(inputId);
+          let labelElement = document.querySelector(`label[for='${inputId}']`);
+          if (this.value !== "") {
+            labelElement.style.fontSize = "1.5em";
+            labelElement.style.color = "var(--purple-second)";
+            input.style.borderColor = "var(--purple-base)";
+          } else {
+            labelElement.style.fontSize = "";
+            labelElement.style.color = "";
+            labelElement.style.margin = "";
+            input.style.borderColor = "";
+          }
+        });
+    }
   }
 });
